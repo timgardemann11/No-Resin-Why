9db8d74539656f46dee6e71d8831716fc8f810bb
timgardemann11 <timgardemann11@gmail.com>
2024-12-30 16:56:44 -0600

    Add files via upload

--- default.php
+++ default.php
@@ -5,15 +5,12 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 include 'functions.php';
+include 'Calendar.php';
+
+$calendar = new Calendar('2024-12-12');
+$calendar->add_event('Holiday', '2024-12-15');
 session_start();
 
-//Pull variables from URL variables
-if (isset($_GET['contentURL'])) {
-    $contentURL = $_GET['contentURL'];
-
-} else {
-    $contentURL = "";
-}
 
 //Get Left Tab Variable
 if (isset($_GET['toptab'])) {
@@ -23,6 +20,24 @@ if (isset($_GET['toptab'])) {
     $toptab = "INV";
 }
 
+//Get search Post
+if (isset($_POST['search'])) {
+    $search = $_POST['search'];
+
+} else {
+    $search = "";
+}
+
+
+//Get Action Variables
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+
+} else {
+    $action = "";
+}
+
+
 
 //Build html Page
 //======================================================================================================Header
@@ -31,6 +46,18 @@ $html = "
     <head>
         <title></title>
         <link href='NRW.css' rel='stylesheet' type='text/css'/>
+        <link rel='stylesheet' href='//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>
+        
+        <script src='https://code.jquery.com/jquery-1.12.4.js'></script>
+		<script src='https://code.jquery.com/ui/1.12.1/jquery-ui.js'></script>
+
+        <script>
+			$( function() {
+			$( '#datepicker' ).datepicker();
+			$( '#datepicker2' ).datepicker();
+			} );
+		</script>
+
     </head>
     <body>
 
@@ -40,27 +67,27 @@ $html = "
 	    		<div id='site'>&nbsp;&nbsp;&nbsp;&nbsp;No Resin Why Operations</div>
     			<div class='menu'>";
     						if($toptab == 'EXP') {
-	    						$html .= "<div class='menu' style='background-color:#C00000;'><a href='default.php?toptab=EXP'>Expenses</a></div>";
+	    						$html .= "<div class='menuactive' style='background-color:#C00000;'><a style='color:white' href='default.php?toptab=EXP'>Expenses</a></div>";
 	    					} else {
 	    						$html .= "<div class='menu' style='background-color:#C00000;'><a href='default.php?toptab=EXP'>Expenses</a></div>";
 	    					}
 	    					if($toptab == 'INV') {
-	    						$html .= "<div class='menu' style='background-color:#4D93D9;'><a href='default.php?toptab=INV'>Inventory</a></div>";
+	    						$html .= "<div class='menuactive' style='background-color:#4D93D9;'><a style='color:white' href='default.php?toptab=INV'>Inventory</a></div>";
 	    					} else {
 	    						$html .= "<div class='menu' style='background-color:#4D93D9;'><a href='default.php?toptab=INV'>Inventory</a></div>";
 	    					}
 							if($toptab == 'SAL') {
-	    						$html .= "<div class='menu' style='background-color:#4EA72E;'><a href='default.php?toptab=SAL'>Sales</a></div>";
+	    						$html .= "<div class='menuactive' style='background-color:#4EA72E;'><a style='color:white' href='default.php?toptab=SAL'>Sales</a></div>";
 	    					} else {
 	    						$html .= "<div class='menu' style='background-color:#4EA72E;'><a href='default.php?toptab=SAL'>Sales</a></div>";
 	    					}
 							if($toptab == 'MLD') {
-	    						$html .= "<div class='menu' style='background-color:#F47E28;'><a href='default.php?toptab=MLD'>Molds</a></div>";
+	    						$html .= "<div class='menuactive' style='background-color:#F47E28;'><a style='color:white' href='default.php?toptab=MLD'>Molds</a></div>";
 	    					} else {
 	    						$html .= "<div class='menu' style='background-color:#F47E28;'><a href='default.php?toptab=MLD'>Molds</a></div>";
 	    					}
 							if($toptab == 'VDR') {
-	    						$html .= "<div class='menu' style='background-color:#9F6CF4;'><a href='default.php?toptab=VDR'>Vendors</a></div>";
+	    						$html .= "<div class='menuactive' style='background-color:#9F6CF4;'><a style='color:white' href='default.php?toptab=VDR'>Vendors</a></div>";
 	    					} else {
 	    						$html .= "<div class='menu' style='background-color:#9F6CF4;'><a href='default.php?toptab=VDR'>Vendors</a></div>";
 	    					}
@@ -90,12 +117,34 @@ $html = "
 		    		</ul>
 				</div>
 			</div>";
-			//---------------------------------------------------------------------Data based on Top Tabs
+			//---------------------------------------------------------------------Company Metrics and Calendar
 			
 			$html .= "
 			<div id='metrics'>
-				<div class='metricsdata'> Metric Data Here</div>
-				<div class='calendar'> Show Schedules and Calendar Here</div>		
+				<div class='metricsdata'>Result of Operations<br>
+				<div style='font-size:medium;color:white;background-color:#4EA72E;width:95%;'>Income</div>
+				<span style='font-size:medium;'>Sales . . . . . . . . . . . $500.00</span><br>
+				<span style='font-size:medium;'>Other Income. . . . . $500.00</span><br>
+				<span style='font-size:medium;font-weight:bold;'>Total Income. . . . . . . . . . $500.00</span><br><br>
+				
+				<div style='font-size:medium;color:white;background-color:#C00000;width:95%;'>Expenses</div>
+				<span style='font-size:medium;'>Molds . . . . . . . . . . . $500.00</span><br>
+				<span style='font-size:medium;'>Color. . . . . . . . . . . . $500.00</span><br>
+				<span style='font-size:medium;'>Embedded . . . . . . . $500.00</span><br>
+				<span style='font-size:medium;'>Resin . . . . . . . . . . . $500.00</span><br>
+				<span style='font-size:medium;'>Show Fees. . . . . . . $500.00</span><br>
+				<span style='font-size:medium;'>Overhead. . . . . . . . $500.00</span><br>
+				<span style='font-size:medium;'>Displays. . . . . . . . . $500.00</span><br>
+				<span style='font-size:medium;font-weight:bold;'>Total Expenses. . . . . . . . . . $500.00</span><br><br>
+				<div style='font-size:medium;color:white;background-color:#4D93D9;width:95%;'>Profit / Loss</div>
+				<span style='font-size:medium;font-weight:bold;'>Total Profit/Loss. . . . . . . . . . $500.00</span><br><br>
+				</div>
+				<div class='calendar'>Upcomming Events:
+				
+					
+				</div>	
+				
+	
 				
 			</div>
 			
@@ -103,15 +152,37 @@ $html = "
 			
 			
 			<div id='data'>";
-			
+			//---------------------------------------------------------------------Data based on Top Tabs
 			switch ($toptab) {
 			
 				case 'EXP':
-					$html .= "<div class='Expense'><div class='datascroll'>";
-					$return = Expenses();
-					$parts = explode("^",$return);
-					$html .= $parts[0];
-					$html .= "$parts[1]</div></div>";
+					$html .= "
+					<div class='Expense'>
+						<div class='datascroll'>";
+							$return = Expenses($search);
+							$parts = explode("^",$return);
+							$html .= $parts[0];
+							$html .= "
+						</div>
+						<div style='float:right;display:inline-block;padding-right:20px;font-size:medium;color:white'>";
+							if($search == ""){$html .= "Showing all Expenses<br>";} else {$html .= "Showing Expenses for search: <span style='font-weight:bold;'>$search</span><br>";}
+							$html .= "<br>
+						 	<form action='Default.php?toptab=$toptab' method='post' enctype='multipart/form-data'>
+								<div class='scontainer'>
+								    <div class='InputContainer'>
+								    	<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='#657789' stroke-width='3' stroke-linecap='round' stroke-linejoin='round' class='feather feather-search'><circle cx='11' cy='11' r='8'/><line x1='21' y1='21' x2='16.65' y2='16.65'/></svg>
+								      <input name='search' id='search' placeholder='Serch Expenses...'/>
+								      <input type='submit' value='Search' name='search1'>
+								    </div>
+								</div>
+							</form><br>
+							<div>
+								Total of Displayed Expenses:  <span style='font-weight:bold;'>$parts[1]</span>
+							</div>
+						 </div>
+						
+						
+					</div>";
 
 					break;
 				case 'INV':
@@ -121,21 +192,86 @@ $html = "
 					
 					break;
 				case 'MLD':
-					$html .= "<div class='Mold'><div class='datascroll'>";
-					$html .= Molds();
-					$html .= "</div></div>";
+					$html .= "
+					<div class='Mold'>
+						<div class='datascroll'>";
+							$return = Molds($search);
+							$parts = explode("^",$return);
+							$html .= $parts[0];
+							$html .= "
+						</div>
+						<div style='float:right;display:inline-block;padding-right:20px;font-size:medium;color:white'>";
+							if($search == ""){$html .= "Showing all Molds<br>";} else {$html .= "Showing Molds for search: <span style='font-weight:bold;'>$search</span><br>";}
+							$html .= "<br>
+						 	<form action='Default.php?toptab=$toptab' method='post' enctype='multipart/form-data'>
+								<div class='scontainer'>
+								    <div class='InputContainer'>
+								    	<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='#657789' stroke-width='3' stroke-linecap='round' stroke-linejoin='round' class='feather feather-search'><circle cx='11' cy='11' r='8'/><line x1='21' y1='21' x2='16.65' y2='16.65'/></svg>
+								      <input name='search' id='search' placeholder='Serch Molds...'/>
+								      <input type='submit' value='Search' name='search1'>
+								    </div>
+								</div>
+							</form><br>
+							<div>
+								Count of Displayed Molds:  <span style='font-weight:bold;'>$parts[1]</span>
+							</div>
+						 </div>
+
+					</div>";
 
 					break;
 				case 'VDR':
-					$html .= "<div class='Vendor'>";
-					$html .= vendors();
-					$html .= "</div>";
+					$html .= "
+					<div class='Vendor'>
+						<div class='datascroll'>";
+							$return = vendors($search);
+							$parts = explode("^",$return);
+							$html .= $parts[0];
+							$html .= "
+						</div>
+						<div style='float:right;display:inline-block;padding-right:20px;font-size:medium;color:white'>";
+							if($search == ""){$html .= "Showing all Vendors<br>";} else {$html .= "Showing Vendors for search: <span style='font-weight:bold;'>$search</span><br>";}
+							$html .= "<br>
+						 	<form action='Default.php?toptab=$toptab' method='post' enctype='multipart/form-data'>
+								<div class='scontainer'>
+								    <div class='InputContainer'>
+								    	<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='#657789' stroke-width='3' stroke-linecap='round' stroke-linejoin='round' class='feather feather-search'><circle cx='11' cy='11' r='8'/><line x1='21' y1='21' x2='16.65' y2='16.65'/></svg>
+								      <input name='search' id='search' placeholder='Serch Vendors...'/>
+								      <input type='submit' value='Search' name='search1'>
+								    </div>
+								</div>
+							</form><br>
+							<div>
+								Count of Displayed Vendors:  <span style='font-weight:bold;'>$parts[1]</span>
+							</div>
+						 </div>
+
+					</div>";
+
 					break;
 			}
 	
 			$html .= "
 			</div>
-		</div>
+		</div>";
+		
+		
+		#======================================================================================================Hidden Forms
+		
+		if($action <> ""){
+			$html .= "<div class='action'></div>
+			<div class='form'>
+				
+				<div class='exit'><a href='default.php?toptab=$toptab'>Exit</a></div>
+				<div class='formtitle'>Add Expenses</div>
+				<div>
+					<form action='Default.php?toptab=$toptab' method='post' enctype='multipart/form-data'>
+						<input type='text' id='datepicker'>
+					</form>	
+			</div>";
+		}
+		
+		$html .= "
 	</body>
 </html>";
 
--- functions.php
+++ functions.php
@@ -1,12 +1,15 @@
 <?php
 
-function Vendors()
+function Vendors($search)
 {
     $return = "<table class='datatable'><tr class='vendorhd'><th>Vendor ID</th><th>Vendor Name</th><th>Account Number</th><th>Location</th></tr>";
     $servername = "nrwphpsite-server.mysql.database.azure.com"; 
   	$username = "tekhneddch"; 
   	$password = "8\$qt3vaajySvWV4U"; 
   	$databasename = "no-resin-why"; 
+  	
+  	$count = 0;
+
     try
     {
 	    // CREATE CONNECTION
@@ -19,7 +22,15 @@ function Vendors()
 		     die("Connection failed: " . $conn->connect_error); 
 		}     
         
-        $sql = "SELECT * FROM `no-resin-why`.vendor";
+        if($search == ""){
+        	$sql = "SELECT * FROM `no-resin-why`.vendor";
+        } else {
+        	$sql = "SELECT * FROM `no-resin-why`.vendor
+					WHERE (lower(VendorName) like lower('%$search%') )
+					or (lower(AccountNum) like lower('%$search%'))
+					or (lower(Location) like lower('%$search%'))";
+        }
+
         
         $result = $conn->query($sql); 
         
@@ -33,6 +44,8 @@ function Vendors()
 				$VAcct = $row["AccountNum"];
 				$VLoc = $row["Location"];
 			    $return .= "<tr><td>$VID</td><td>$VName</td><td>$VAcct</td><td>$VLoc</td></tr>";
+			    
+			    $count++;
 			}
 		} else {
 			$return .= "<tr><td>No results</td></tr>";
@@ -46,11 +59,11 @@ function Vendors()
         echo("Error!");
     }
 
-    return $return;
+    return "$return^$count";
 }
 
 
-function Expenses()
+function Expenses($search)
 {
     $return = "<table class='datatable'><tr class='expensehd'><th>Expense ID</th><th>Date</th><th>Vendor</th><th>Description</th><th>Amount</th></tr>";
     $servername = "nrwphpsite-server.mysql.database.azure.com"; 
@@ -71,7 +84,15 @@ function Expenses()
 		     die("Connection failed: " . $conn->connect_error); 
 		}     
         
-        $sql = "SELECT * FROM `no-resin-why`.expenses";
+        if($search == ""){
+        	$sql = "SELECT * FROM `no-resin-why`.expenses";
+        } else {
+        	$sql = "SELECT * FROM `no-resin-why`.expenses
+					WHERE (lower(Vendor) like lower('%$search%') )
+					or (lower(Description) like lower('%$search%'))
+					or (Date like '%$search%')
+					or (Amount like '%$search%')";
+        }
         
         $result = $conn->query($sql); 
         
@@ -107,13 +128,15 @@ function Expenses()
 
 
 
-function Molds()
+function Molds($search)
 {
-    $return = "<table class='datatable'><tr class='moldhd'><th>Expense ID</th><th>Date</th><th>Vendor</th><th>Description</th><th>Amount</th></tr>";
+    $return = "<table class='datatable'><tr class='moldhd'><th>Mold ID</th><th>Shape</th><th>Description</th><th>Size</th></tr>";
     $servername = "nrwphpsite-server.mysql.database.azure.com"; 
   	$username = "tekhneddch"; 
   	$password = "8\$qt3vaajySvWV4U"; 
   	$databasename = "no-resin-why"; 
+  	
+  	$count = 0;
   	
     try
     {
@@ -128,7 +151,15 @@ function Molds()
 		}     
         
         $sql = "SELECT * FROM `no-resin-why`.molds";
-        
+        if($search == ""){
+        	$sql = "SELECT * FROM `no-resin-why`.molds";
+        } else {
+        	$sql = "SELECT * FROM `no-resin-why`.molds
+					WHERE (lower(Shape) like lower('%$search%') )
+					or (lower(Description) like lower('%$search%'))
+					or (lower(Size) like lower('%$search%'))";
+        }
+
         $result = $conn->query($sql); 
         
         if ($result->num_rows > 0)
@@ -140,7 +171,8 @@ function Molds()
 				$Shape = $row["Shape"];
 				$Description = $row["Description"];
 				$Size = $row["Size"];
-
+				
+				$count++;
 			    $return .= "<tr><td>$MID</td><td>$Shape</td><td>$Description</td><td>$Size</td></tr>";
 			    
 			}
@@ -156,8 +188,13 @@ function Molds()
         echo("Error!");
     }
 
-    return "$return";
+    return "$return^$count";
 }
 
+
+function AddEvent(){
+
+
+}
 
 ?>
