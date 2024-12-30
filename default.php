3b36a0742d5d4ce9a17bc84bbf4d7bb556a8356d
timgardemann11 <timgardemann11@gmail.com>
2024-12-29 18:11:33 -0600

    Add files via upload

--- default.php
+++ default.php
@@ -26,70 +26,117 @@ if (isset($_GET['toptab'])) {
 
 //Build html Page
 //======================================================================================================Header
-    $html = "
-    <html>
+$html = "
+<html>
     <head>
         <title></title>
         <link href='NRW.css' rel='stylesheet' type='text/css'/>
     </head>
     <body>
-    <div id='main'>
-    	<table class='top'>
-    		<tr>
-    			<td><img src='images/Logo.png' alt='No Resin Why' style='width:100px;'></td>
-    			<td><div id='site'>No Resin Why Operations</div></td>
-    			<td><div class='menu'>
-    				<ul>";
-    					if($toptab == 'EXP') {
-	    						$html .= "<li style='background-color:#C00000;'><a href='default.php?toptab=EXP'>Expenses</a></li>";
+
+	    <div id='main'>
+	    	
+	    		<div id='logo'>&nbsp;&nbsp;<img src='images/SiteLogo.png' alt='No Resin Why' style='width:120px;'></div>
+	    		<div id='site'>&nbsp;&nbsp;&nbsp;&nbsp;No Resin Why Operations</div>
+    			<div class='menu'>";
+    						if($toptab == 'EXP') {
+	    						$html .= "<div class='menu' style='background-color:#C00000;'><a href='default.php?toptab=EXP'>Expenses</a></div>";
 	    					} else {
-	    						$html .= "<li style='background-color:#C00000;'><a href='default.php?toptab=EXP'>Expenses</a></li>";
+	    						$html .= "<div class='menu' style='background-color:#C00000;'><a href='default.php?toptab=EXP'>Expenses</a></div>";
 	    					}
 	    					if($toptab == 'INV') {
-	    						$html .= "<li style='background-color:#4D93D9;'><a href='default.php?toptab=INV'>Inventory</a></li>";
+	    						$html .= "<div class='menu' style='background-color:#4D93D9;'><a href='default.php?toptab=INV'>Inventory</a></div>";
 	    					} else {
-	    						$html .= "<li style='background-color:#4D93D9;'><a href='default.php?toptab=INV'>Inventory</a></li>";
+	    						$html .= "<div class='menu' style='background-color:#4D93D9;'><a href='default.php?toptab=INV'>Inventory</a></div>";
 	    					}
 							if($toptab == 'SAL') {
-	    						$html .= "<li style='background-color:#4EA72E;'><a href='default.php?toptab=SAL'>Sales</a></li>";
+	    						$html .= "<div class='menu' style='background-color:#4EA72E;'><a href='default.php?toptab=SAL'>Sales</a></div>";
 	    					} else {
-	    						$html .= "<li style='background-color:#4EA72E;'><a href='default.php?toptab=SAL'>Sales</a></li>";
+	    						$html .= "<div class='menu' style='background-color:#4EA72E;'><a href='default.php?toptab=SAL'>Sales</a></div>";
 	    					}
 							if($toptab == 'MLD') {
-	    						$html .= "<li style='background-color:#F47E28;'><a href='default.php?toptab=MLD'>Molds</a></li>";
+	    						$html .= "<div class='menu' style='background-color:#F47E28;'><a href='default.php?toptab=MLD'>Molds</a></div>";
 	    					} else {
-	    						$html .= "<li style='background-color:#F47E28;'><a href='default.php?toptab=MLD'>Molds</a></li>";
+	    						$html .= "<div class='menu' style='background-color:#F47E28;'><a href='default.php?toptab=MLD'>Molds</a></div>";
 	    					}
 							if($toptab == 'VDR') {
-	    						$html .= "<li style='background-color:#9F6CF4;'><a href='default.php?toptab=VDR'>Vendors</a></li>";
+	    						$html .= "<div class='menu' style='background-color:#9F6CF4;'><a href='default.php?toptab=VDR'>Vendors</a></div>";
 	    					} else {
-	    						$html .= "<li style='background-color:#9F6CF4;'><a href='default.php?toptab=VDR'>Vendors</a></li>";
+	    						$html .= "<div class='menu' style='background-color:#9F6CF4;'><a href='default.php?toptab=VDR'>Vendors</a></div>";
 	    					}
 						$html .= "
-    				</ul>
-    			<td>
-        		<td><div id='catchphrase'>Get Yours Today! 555.123.5555&nbsp;&nbsp;</div></td>
-        		<td><div class='round'><a href='default.php?contentURL=login'>Login to Order Online</a></div><td>
-        	</tr>
-        </table>
-    </div>";
+ 	    		</div>
+	        	
+	      </div>";
+	
+		//======================================================================================================Body
+			//---------------------------------------------------------------------Controls on the left
+		$html .= "
+		<div id='container'>
+			<div id='controls'><br>
+					
+				<div class='sidemenu'>
+		   			<ul>
+		   					<li><a href='default.php?action=EXP&toptab=$toptab'>Add New Expense</a></li>
+		   					<li><a href='default.php?acion=ITM&toptab=$toptab'>Add New Items</a></li>
+							<li><a href='default.php?action=SLD&toptab=$toptab'>Mark Items as Sold</a></li>
+							<li><a href='default.php?action=MLD&toptab=$toptab'>Add New Mold</a></li>
+							<li><a href='default.php?action=MLD&toptab=$toptab'>Add Mold Size</a></li>
+							<li><a href='default.php?actopm=VDR&toptab=$toptab'>Add New Vendor</a></li>
+							<li><a href='default.php?actopm=VDR&toptab=$toptab'>Schedule Show</a></li>
+							<li><a href='default.php?actopm=VDR&toptab=$toptab'>Add Calendar Event</a></li>
 
-//======================================================================================================Body
-	$html .= "<div id='controls'><br>
-			
-       			<div class='sidemenu'>
-	       			<ul>
-	       					<li><a href='default.php?action=EXP&toptab=$toptab'>Add New Expense</a></li>
-	       					<li><a href='default.php?acion=ITM&toptab=$toptab'>Add New Item</a></li>
-	    					<li><a href='default.php?action=SLD&toptab=$toptab'>Mark Items as Sold</a></li>
-	    					<li><a href='default.php?action=MLD&toptab=$toptab'>Add New Mold</a></li>
-	    					<li><a href='default.php?actopm=VDR&toptab=$toptab'>Add New Vendor</a></li>
-	    					
-	        		</ul>
+							
+		    		</ul>
 				</div>
 			</div>";
-        
-    $html .= "</body>
+			//---------------------------------------------------------------------Data based on Top Tabs
+			
+			$html .= "
+			<div id='metrics'>
+				<div class='metricsdata'> Metric Data Here</div>
+				<div class='calendar'> Show Schedules and Calendar Here</div>		
+				
+			</div>
+			
+			
+			
+			
+			<div id='data'>";
+			
+			switch ($toptab) {
+			
+				case 'EXP':
+					$html .= "<div class='Expense'><div class='datascroll'>";
+					$return = Expenses();
+					$parts = explode("^",$return);
+					$html .= $parts[0];
+					$html .= "$parts[1]</div></div>";
+
+					break;
+				case 'INV':
+					
+					break;
+				case 'SAL':
+					
+					break;
+				case 'MLD':
+					$html .= "<div class='Mold'><div class='datascroll'>";
+					$html .= Molds();
+					$html .= "</div></div>";
+
+					break;
+				case 'VDR':
+					$html .= "<div class='Vendor'>";
+					$html .= vendors();
+					$html .= "</div>";
+					break;
+			}
+	
+			$html .= "
+			</div>
+		</div>
+	</body>
 </html>";
 
 echo $html
--- functions.php
+++ functions.php
@@ -1,361 +1,163 @@
 <?php
 
-function OpenConnection()
+function Vendors()
 {
+    $return = "<table class='datatable'><tr class='vendorhd'><th>Vendor ID</th><th>Vendor Name</th><th>Account Number</th><th>Location</th></tr>";
+    $servername = "nrwphpsite-server.mysql.database.azure.com"; 
+  	$username = "tekhneddch"; 
+  	$password = "8\$qt3vaajySvWV4U"; 
+  	$databasename = "no-resin-why"; 
     try
     {
-        $serverName = "TIM-7\SQLEXPRESS";
-        $connectionOptions = array("Database"=>"Store",
-            "Uid"=>"sa", "PWD"=>"PatTim");
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-        if($conn == false)
-            die("Failed to connect");
-    }
+	    // CREATE CONNECTION
+	    //$conn = mysqli_init(); 
+	    //mysqli_real_connect($conn,$servername,$username,$password,$databasename,3306,MYSQLI_CLIENT_SSL);
+	  	$conn = new mysqli($servername, $username, $password, $databasename);
+	    	
+		// GET CONNECTION ERRORS 
+		if ($conn->connect_error) { 
+		     die("Connection failed: " . $conn->connect_error); 
+		}     
+        
+        $sql = "SELECT * FROM `no-resin-why`.vendor";
+        
+        $result = $conn->query($sql); 
+        
+        if ($result->num_rows > 0)
+		{		
+			// Loop through each row in the result set
+			while($row = $result->fetch_assoc())
+			{
+				$VID = $row["VendorID"];
+				$VName = $row["VendorName"];
+				$VAcct = $row["AccountNum"];
+				$VLoc = $row["Location"];
+			    $return .= "<tr><td>$VID</td><td>$VName</td><td>$VAcct</td><td>$VLoc</td></tr>";
+			}
+		} else {
+			$return .= "<tr><td>No results</td></tr>";
+		}
+		$return .= "</table>";
+        // Close connections
+		mysqli_close($conn); 
+	}   
     catch(Exception $e)
     {
         echo("Error!");
     }
-}
 
-
-function ReadData($id)
-{
-    $return = "";
-    try
-    {
-        $serverName = "TIM-7\SQLEXPRESS";
-        $connectionOptions = array("Database"=>"Store",
-            "Uid"=>"sa", "PWD"=>"PatTim");
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-
-
-        $tsql = "SELECT        Items.ItemID, Items.ItemName, Items.ItemDesc, Items.PicturePath, Items.MarkupDollars, CalcTable.Cost, Stores.StoreID
-			                        , Stores.StoreMarkup, CalcTable.Cost +(CalcTable.Cost * (Stores.StoreMarkup / 100)) + Items.MarkupDollars as SuggestedRetail
-                    FROM            Items INNER JOIN
-                             (SELECT        SUM(ExpenseAmount) AS Cost, ItemID
-                               FROM            Expenses
-                               GROUP BY ItemID) AS CalcTable ON Items.ItemID = CalcTable.ItemID INNER JOIN
-                         Stores ON Items.StoreID = Stores.StoreID
-                    WHERE Stores.StoreID = {$id}";
-        $getProducts = sqlsrv_query($conn, $tsql);
-        if ($getProducts == FALSE)
-            die("Failed to connect");
-        $productCount = 0;
-        while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
-        {
-            $return .= $row['ItemName'];
-            $return .= ",";
-            $return .= $row['ItemDesc'];
-            $return .= ",";
-            $return .= $row['PicturePath'];
-            $return .= ",";
-            $return .= $row['SuggestedRetail'];
-            $return .= ":";
-            //echo("<br/>");
-            $productCount++;
-        }
-        sqlsrv_free_stmt($getProducts);
-        sqlsrv_close($conn);
-    }
-    catch(Exception $e)
-    {
-        echo("Error!");
-    }
-    $return =substr($return,0,-1);
     return $return;
 }
 
 
-function InsertData()
+function Expenses()
 {
+    $return = "<table class='datatable'><tr class='expensehd'><th>Expense ID</th><th>Date</th><th>Vendor</th><th>Description</th><th>Amount</th></tr>";
+    $servername = "nrwphpsite-server.mysql.database.azure.com"; 
+  	$username = "tekhneddch"; 
+  	$password = "8\$qt3vaajySvWV4U"; 
+  	$databasename = "no-resin-why"; 
+  	
+  	$Total = 0;
     try
     {
-        $conn = OpenConnection();
-        $tsql = "INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT            INSERTED.ProductID VALUES ('SQL Server 1', 'SQL Server 2', 0, 0, getdate())";
-        //Insert query
-        $insertReview = sqlsrv_query($conn, $tsql);
-        if($insertReview == FALSE)
-            die("Failed to connect");
-        echo "Product Key inserted is :";
-        while($row = sqlsrv_fetch_array($insertReview, SQLSRV_FETCH_ASSOC))
-        {
-            echo($row['ProductID']);
-        }
-        sqlsrv_free_stmt($insertReview);
-        sqlsrv_close($conn);
-    }
+	    // CREATE CONNECTION
+	    //$conn = mysqli_init(); 
+	    //mysqli_real_connect($conn,$servername,$username,$password,$databasename,3306,MYSQLI_CLIENT_SSL);
+	  	$conn = new mysqli($servername, $username, $password, $databasename);
+	    	
+		// GET CONNECTION ERRORS 
+		if ($conn->connect_error) { 
+		     die("Connection failed: " . $conn->connect_error); 
+		}     
+        
+        $sql = "SELECT * FROM `no-resin-why`.expenses";
+        
+        $result = $conn->query($sql); 
+        
+        if ($result->num_rows > 0)
+		{		
+			// Loop through each row in the result set
+			while($row = $result->fetch_assoc())
+			{
+				$EID = $row["ExpenseID"];
+				$EDate = $row["Date"];
+				$EVendor = $row["Vendor"];
+				$EDescription = $row["Description"];
+				$EAmount = $row["Amount"];
+
+			    $return .= "<tr><td>$EID</td><td>$EDate</td><td>$EVendor</td><td>$EDescription</td><td>$EAmount</td></tr>";
+			    
+			    $Total = $Total + $EAmount;
+			}
+		} else {
+			$return .= "<tr><td>No results</td></tr>";
+		}
+		$return .= "</table>";
+        // Close connections
+		mysqli_close($conn); 
+	}   
     catch(Exception $e)
     {
         echo("Error!");
     }
+
+    return "$return^$Total";
 }
 
 
-function logindata($username,$password)
+
+function Molds()
 {
-    $return = "";
+    $return = "<table class='datatable'><tr class='moldhd'><th>Expense ID</th><th>Date</th><th>Vendor</th><th>Description</th><th>Amount</th></tr>";
+    $servername = "nrwphpsite-server.mysql.database.azure.com"; 
+  	$username = "tekhneddch"; 
+  	$password = "8\$qt3vaajySvWV4U"; 
+  	$databasename = "no-resin-why"; 
+  	
     try
     {
-        $serverName = "TIM-7\SQLEXPRESS";
-        $connectionOptions = array("Database"=>"Store",
-            "Uid"=>"sa", "PWD"=>"PatTim");
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-
-
-        $tsql = "SELECT [StoreID]
-                        ,[Name]
-                        ,[Username]
-                        ,[Password]
-                        ,[Rights]
-      
-                 FROM [Store].[dbo].[Owners]
-                 Where [Username] = '{$username}' and [Password] = '{$password}'";
-        $getStoreIDs = sqlsrv_query($conn, $tsql);
-        if ($getStoreIDs == FALSE) 
-            $return = "Failed to query database ".sqlsrv_error();
-         
-        $idCount = 0;
-        while($row = sqlsrv_fetch_array($getStoreIDs, SQLSRV_FETCH_ASSOC))
-        {
-            $return .= $row['StoreID'];
-            $return .= ",";
-            //$return .= $row['Rights'];
-            //$return .= ":";
-            //echo("<br/>");
-            $idCount++;
-            $ownername = $row['Name'];
-        }
+	    // CREATE CONNECTION
+	    //$conn = mysqli_init(); 
+	    //mysqli_real_connect($conn,$servername,$username,$password,$databasename,3306,MYSQLI_CLIENT_SSL);
+	  	$conn = new mysqli($servername, $username, $password, $databasename);
+	    	
+		// GET CONNECTION ERRORS 
+		if ($conn->connect_error) { 
+		     die("Connection failed: " . $conn->connect_error); 
+		}     
         
-        sqlsrv_free_stmt($getStoreIDs);
-        sqlsrv_close($conn);
-        $_SESSION["ownername"]=$ownername;
-    }
-    catch(Exception $e)
-    {
-        echo("Error!");
-    }
-
-    if ($idCount == 0) {
-        $return = "Username or Password Incorrect, Login Failed!";
-    } else {
-        $return =substr($return,0,-1);
+        $sql = "SELECT * FROM `no-resin-why`.molds";
         
-        return $return;
-    }
-}
-
-function getcity ($selectedstate,$selectedcity)
-{
-
-
-$return = "";
-     try
-    {
-        $serverName = "TIM-7\SQLEXPRESS";
-        $connectionOptions = array("Database"=>"Store",
-            "Uid"=>"sa", "PWD"=>"PatTim");
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-        $tsql = "SELECT [CityID]
-                        ,[City]
-                    FROM [Store].[dbo].[Cities]
-                    WHERE [StateID] = {$selectedstate}";
-        $getCities = sqlsrv_query($conn, $tsql);
-        if ($getCities == FALSE) 
-            echo "Failed to query database ".sqlsrv_error();
-         
-        while($row = sqlsrv_fetch_array($getCities, SQLSRV_FETCH_ASSOC))
-        {   
-            $tempID = $row['CityID'];
-            $tempcity = $row['City'];
-
-            if($tempID == $selectedcity){
-                $return .= "<option value='{$tempID}' selected='selected'>{$tempcity}</option>";
-            }else if ($tempID != $selectedcity) {
-                $return .= "<option value='{$tempID}'>{$tempcity}</option>";
-            }    
-        }
+        $result = $conn->query($sql); 
         
-        sqlsrv_free_stmt($getCities);
-        sqlsrv_close($conn);
-    }
+        if ($result->num_rows > 0)
+		{		
+			// Loop through each row in the result set
+			while($row = $result->fetch_assoc())
+			{
+				$MID = $row["MoldID"];
+				$Shape = $row["Shape"];
+				$Description = $row["Description"];
+				$Size = $row["Size"];
+
+			    $return .= "<tr><td>$MID</td><td>$Shape</td><td>$Description</td><td>$Size</td></tr>";
+			    
+			}
+		} else {
+			$return .= "<tr><td>No results</td></tr>";
+		}
+		$return .= "</table>";
+        // Close connections
+		mysqli_close($conn); 
+	}   
     catch(Exception $e)
     {
         echo("Error!");
     }
-        return $return;
+
+    return "$return";
 }
 
 
-
-function getstate ($selectedstate)
-{
-
-if($stateID = ""){$stateID = 0;}
-$return = "";
-     try
-    {
-        $serverName = "TIM-7\SQLEXPRESS";
-        $connectionOptions = array("Database"=>"Store",
-            "Uid"=>"sa", "PWD"=>"PatTim");
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-        $tsql = "SELECT [StateID]
-                        ,[State]
-                    FROM [Store].[dbo].[State]";
-        $getStates = sqlsrv_query($conn, $tsql);
-        if ($getStates == FALSE) 
-            echo "Failed to query database ".sqlsrv_error();
-        while($row = sqlsrv_fetch_array($getStates, SQLSRV_FETCH_ASSOC))
-        {   
-            $tempID = $row['StateID'];
-            $tempState = $row['State'];
-            if($tempID == $selectedstate){
-                $return .= "<option value='{$tempID}' selected='selected'>{$tempState}</option>";
-            }else if ($tempID != $selectedstate) {
-                $return .= "<option value='{$tempID}'>{$tempState}</option>";
-            }      
-        }
-        
-        sqlsrv_free_stmt($getStates);
-        sqlsrv_close($conn);
-    }
-    catch(Exception $e)
-    {
-        echo("Error!");
-    }
-        return $return;
-}
-
-
-
-function getstores ($selectedcity,$selectedstore)
-{
-$return = "";
-    try
-    {
-        $serverName = "TIM-7\SQLEXPRESS";
-        $connectionOptions = array("Database"=>"Store",
-            "Uid"=>"sa", "PWD"=>"PatTim");
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-        $tsql = "SELECT [StoreID]
-                        ,[StoreName]
-                    FROM [Store].[dbo].[Stores]
-                    WHERE [CityID] = {$selectedcity}";
-        $getStores = sqlsrv_query($conn, $tsql);
-        if ($getStores == FALSE) 
-            $return = "Failed to query database ";
-        while($row = sqlsrv_fetch_array($getStores, SQLSRV_FETCH_ASSOC))
-        {   
-            $tempID = $row['StoreID'];
-            $tempstore = $row['StoreName'];
-            if($tempID == $selectedstore){
-                $return .= "<option value='{$tempID}' selected='selected'>{$tempstore}</option>";               
-                $contentURL = "storecontent.php?id=$tempID&sn=$tempstore";
-            }else if ($tempID != $selectedstore) {
-                $return .= "<option value='{$tempID}'>{$tempstore}</option>";   
-            }
-        }
-        sqlsrv_free_stmt($getStores);
-        sqlsrv_close($conn);
-        $return .= "</select>";
-    }
-    catch(Exception $e)
-    {
-        echo("Error!");
-    }
-    return "$return:$contentURL";
-}
-
-function getownerstores($stores,$selectedstore)
-{
-$return = "";
-try
-    {
-        $serverName = "TIM-7\SQLEXPRESS";
-        $connectionOptions = array("Database"=>"Store",
-            "Uid"=>"sa", "PWD"=>"PatTim");
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-        $tsql = "SELECT [StoreID]
-                        ,[StoreName]
-                    FROM [Store].[dbo].[Stores]
-                    WHERE [StoreID] IN ({$stores})";
-        $getStores = sqlsrv_query($conn, $tsql);               
-        while($row = sqlsrv_fetch_array($getStores, SQLSRV_FETCH_ASSOC))
-        {   
-            $tempID = $row['StoreID'];
-            $tempstore = $row['StoreName'];
-
-
-            if (substr($stores, "," !== false)) {
-                
-                if($tempID == $selectedstore){
-                    $return .= "<option value='{$tempID}' selected='selected'>{$tempstore}</option>";             
-                    $contentURL = "storeowner.php?id=$tempID&sn=$tempstore";
-                }else if ($tempID != $selectedstore) {
-                    $return .= "<option value='{$tempID}'>{$tempstore}</option>";   
-                }
-            }else {
-                $return .= "<option value='{$tempID}' selected='selected'>{$tempstore}</option>";             
-                $contentURL = "storeowner.php?id=$tempID&sn=$tempstore"; 
-            }
-            
-
-        }
-        sqlsrv_free_stmt($getStores);
-        sqlsrv_close($conn);
-        $return .= "</select>";
-    }
-    catch(Exception $e)
-    {
-        echo("Error!");
-    } 
-return "$return:$contentURL";
-}
-
-
-function getMenu ($menuid)
-{
-    
-    
-
-    $return = "";
-     try
-    {
-        $serverName = "TIM-7\SQLEXPRESS";
-        $connectionOptions = array("Database"=>"Store",
-            "Uid"=>"sa", "PWD"=>"PatTim");
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-
-
-        $tsql = "SELECT [MenuText]
-                        ,[MenuAction]
-                        ,[MenuIcon]
-                    FROM [Store].[dbo].[Menu]
-                    WHERE [MenuID] = {$menuid}";
-        $getMenus = sqlsrv_query($conn, $tsql);
-        if ($getMenus == FALSE) 
-            $return = "Failed to query database ";
-         
-        while($row = sqlsrv_fetch_array($getMenus, SQLSRV_FETCH_ASSOC))
-        {   
-            $menutext = $row['MenuText'];
-            $menuaction = $row['MenuAction'];
-            $menuicon = $row['MenuIcon'];
-
-            $return .= "<li><a href='{$menuaction}' target='content'>{$menutext}</a></li>";
-            
-        }
-        
-        sqlsrv_free_stmt($getMenus);
-        sqlsrv_close($conn);
-    }
-    catch(Exception $e)
-    {
-        echo("Error!");
-    }
-        $return .= "</ul>";
-        return $return;
-
-
-
-}
 ?>
