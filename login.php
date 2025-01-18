0feb71b4874e6f93c06ef93c671ac687ddd1a434
timgardemann11 <timgardemann11@gmail.com>
2025-01-18 09:20:15 -0600

    Add files via upload

--- login.php
+++ login.php
@@ -17,6 +17,7 @@ $html = "
 <title>Logon Page</title>
 
 <link href='NRWLogon.css' rel='stylesheet' type='text/css'/>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
 
 </head>
 
