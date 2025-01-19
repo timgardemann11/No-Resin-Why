1ad6c663b42476154ec0cdd92990c27de525d244
timgardemann11 <timgardemann11@gmail.com>
2025-01-19 11:29:13 -0600

    Add files via upload

--- mobile.php
+++ mobile.php
@@ -25,6 +25,34 @@ If (isset($_GET['action'])) {
 }
 
 
+if (isset($_POST['subsho'])) {
+	$name = $_POST['name'];
+	$date = $_POST['sdate'];
+	$start = $_POST['start'];
+	$finish = $_POST['finish'];
+	$locaddress = $_POST['locationaddress'];
+	$loccity = $_POST['locationcity'];
+	$locstate = $_POST['locationstate'];
+	$conname = $_POST['contactname'];
+	$conemail = $_POST['contactemail'];
+	$conphone = $_POST['contactphone'];
+	$location = $_POST['location'];
+		$result = AddShow($name,$date,$start,$finish,$locaddress,$loccity,$locstate,$conname,$conemail,$conphone,$location);
+} else {
+	$name = "";
+	$date = "";
+	$start = "";
+	$finish = "";
+	$locaddress = "";
+	$loccity = "";
+	$locstate = "";
+	$conname = "";
+	$conemail = "";
+	$conphone = "";
+	$location = "";
+}
+
+
 //Build Logon Page
 $html = "
 
@@ -34,25 +62,47 @@ $html = "
 
 <link href='NRWMobile.css' rel='stylesheet' type='text/css'/>
 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<link rel='stylesheet' href='//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>
+
+<script src='https://code.jquery.com/jquery-1.12.4.js'></script>
+<script src='https://code.jquery.com/ui/1.12.1/jquery-ui.js'></script>
+
+<script>
+	$( function() {
+	$( '#datepicker' ).datepicker();
+	$( '#datepicker2' ).datepicker();
+	} );
+</script>
 
 </head>
 
 <body>";
 
 //======================================================================================================Body
-			//---------------------------------------------------------------------Controls on the left
+			//---------------------------------------------------------------------Mobile Page Header
 		$html .= "
 		<div id='container'>
 			<div id='logo'>
 				<table width='400'>
 					<tr>
-						<td rowspan='2' style='width:270px;'><img src='images/SiteLogo.png' alt='No Resin Why' style='max-width:50%;height:auto;'></td>
-						<td rowspan='2'><img src='images/user.png'></td>
+						<td rowspan='2' style='width:33%;'><img src='images/SiteLogo.png' alt='No Resin Why' style='max-width:100%;height:auto;'></td>
+						<td rowspan='2' style='width:33%;text-align:right;'><img src='images/user.png'></td>
 						<td class='user' style='vertical-align:bottom;'>$user</td>
 					</tr>
 					<tr>
 						<td class='user' style='vertical-align:top;'>$title</td>
 					</tr>
+					<tr colspan='3'>
+						<table class='siteswitcherheader'>
+							<tr>
+								<td class='siteswitcherheader' colspan='3' style='vertical-align:bottom;text-align:center;height:10px;'>Site Switcher</td>
+							<tr class='siteswitcherheader'>
+								<td><a href='mobile.php'><div class='siteswitcheractive'>Mobile</div></a></td>
+								<td><a href='default.php'><div class='siteswitcher'>Operations</div></a></td>
+								<td><a href='show.php'><div class='siteswitcher'>Craft Show</div></a></td>
+							</tr>
+						</table>
+					</tr>							
 				</table>";
 			
 			Switch ($action) {
@@ -102,7 +152,7 @@ $html = "
 										
 										$formparts[2]
 										<br>
-										<a href='mobile.php'><div class='exit'>Cancel</div></a><input class='subbutton' type='submit' id='$subid' name='$subid' value='$formparts[5]'>
+										<a href='mobile.php'><div class='exit'>Cancel</div></a><input class='subbutton' type='submit' id='$formparts[4]' name='$formparts[4]' value='$formparts[5]'>
 										$formparts[3]					
 									</form>	
 							</div>
